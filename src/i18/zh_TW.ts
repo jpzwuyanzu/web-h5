f29ef6fceddf781f4c5584b3d06ebdd651f0a09c
simon <simon@gmail.com>
2024-12-24 15:27:58 +0700

    fix: 修复多语言问题

---
+++ src/i18/zh_TW.ts
@@ -0,0 +1,26 @@
+export default {
+    home: {
+        perPage: "上一頁",
+        nextPage: "下一頁",
+        searchPlaceholder: "請輸入你想要搜尋的內容"
+    },
+    sideMenu: {
+        langue: {
+            zh_CN: "簡體中文",
+            zh_TW: "繁體中文",
+            en_US: "English",
+            ja_JP: "日本語",
+        },
+        menuInfo: {
+            mainMenu: "主選單",
+            menu_1: "選單1",
+            menu_2: "選單2",
+            menu_3: "選單3"
+        }
+    },
+    routeLinkName: {
+        home: "首頁",
+        video: "影片",
+        detail: "影片詳情"
+    }
+}
--- src/lang.ts
+++ src/lang.ts
@@ -1,11 +1,14 @@
 import { createI18n } from 'vue-i18n'
 import zh_CN from "@/i18/zh_CN"
 import en_US from "@/i18/en_US"
+import zh_TW from '@/i18/zh_TW'
 // Import default CSS
 
+console.log(zh_TW)
 const messages = {
     en_US: { ...en_US },
-    zh_CN: { ...zh_CN }
+    zh_CN: { ...zh_CN },
+    zh_TW: { ...zh_TW },
 }
 
 export default createI18n({
